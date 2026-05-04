cd63ad849ff2bc24df4d860352a8b02af6eeac0e
Robertas Grajevskis <robertas.grajevskis@gmail.com>
2026-05-04 23:23:09 +0300

    Add LMMA sponsor logo

--- template-parts/sponsors-section.php
+++ template-parts/sponsors-section.php
@@ -23,7 +23,7 @@
 
     @media screen and (min-width: 801px) {
         .sponsors {
-            grid-template-columns: repeat(4, 1fr);
+            grid-template-columns: repeat(5, 1fr);
         }
     }
 </style>
@@ -35,4 +35,5 @@
     <a href='https://www.ltkt.lt/' target='_blank' rel='noopener noreferrer'><img src='https://lchs.lt/wp-content/uploads/ltkt-LOGO.png' alt='LTKT'></a>
     <a href='https://vilnius.lt/lt/' target='_blank' rel='noopener noreferrer'><img src='https://lchs.lt/wp-content/uploads/VILNIUS_RED_TRANSPARENT_RGB.png' alt='Vilnius'></a>
     <a href='https://www.latga.lt/' target='_blank' rel='noopener noreferrer'><img src='https://lchs.lt/wp-content/uploads/Latga_logotipas_juodas_RGB.png' alt='LATGA'></a>
+    <a href='https://www.lmma.eu/' target='_blank' rel='noopener noreferrer'><img src='https://lchs.lt/wp-content/uploads/lmma-logo.jpg' alt='LMMA'></a>
 </div>
