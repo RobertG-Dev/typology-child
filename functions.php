109e0b6626d9cb08913590dc00bde644ec44b102
RobertG-Dev <robertas.grajevskis@gmail.com>
2026-07-10 00:05:57 +0300

    chore: remove comments from theme

--- functions.php
+++ functions.php
@@ -18,64 +18,6 @@ function typology_child_load_scripts()
     wp_enqueue_style('typology_child_style');
 }
 
-// Completely disable comments across the entire site
-
-// Disable support for comments and trackbacks in post types
-function disable_comments_post_types()
-{
-    $post_types = get_post_types();
-    foreach ($post_types as $post_type) {
-        if (post_type_supports($post_type, 'comments')) {
-            remove_post_type_support($post_type, 'comments');
-            remove_post_type_support($post_type, 'trackbacks');
-        }
-    }
-}
-
-add_action('admin_init', 'disable_comments_post_types');
-
-// Close comments on the front-end
-function disable_comments_status()
-{
-    return false;
-}
-
-add_filter('comments_open', 'disable_comments_status', 20, 2);
-add_filter('pings_open', 'disable_comments_status', 20, 2);
-
-// Hide existing comments
-function hide_existing_comments($comments)
-{
-    return array();
-}
-
-add_filter('comments_array', 'hide_existing_comments', 10, 2);
-
-// Remove comments from admin menu
-function remove_comments_admin_menu()
-{
-    remove_menu_page('edit-comments.php');
-}
-
-add_action('admin_menu', 'remove_comments_admin_menu');
-
-// Remove comments metabox from dashboard
-function remove_comments_dashboard()
-{
-    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
-}
-
-add_action('admin_init', 'remove_comments_dashboard');
-
-// Remove comments links from admin bar
-function remove_comments_admin_bar()
-{
-    global $wp_admin_bar;
-    $wp_admin_bar->remove_menu('comments');
-}
-
-add_action('wp_before_admin_bar_render', 'remove_comments_admin_bar');
-
 function typology_get_archive_heading()
 {
     $defaults = array(
